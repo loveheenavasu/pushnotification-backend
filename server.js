9eb26b1f1ecd0fef20974b91bd361599aaebd136
loveheenavasu <trivediheenalove@gmail.com>
2023-10-12 10:34:05 +0530

    backend changes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000; // Set your desired port
 const cors = require("cors"); // Import the cors middleware
 
 const admin = require("firebase-admin");
-const getmessage = require("firebase-admin/messaging")
+const getmessage = require("firebase-admin/messaging");
 
 const serviceAccount = require("./notification-23222-firebase-adminsdk-v8fmn-a2aff1f562.json");
 app.use(cors()); // Use cors middleware
@@ -33,16 +33,19 @@ app.post("/send-notification", async (req, res) => {
     };
     console.log(req.body, "req.body");
     // Send a push notification using the axios library or Firebase Admin SDK
-    getmessage.getMessaging().sendEachForMulticast(message)
-    .then((response) => {
-      console.log(response + ' messages were sent successfully');
-    });
+    getmessage
+      .getMessaging()
+      .sendEachForMulticast(message)
+      .then((response) => {
+        console.log(response.responses, "----reponse----", response.successCount,"---count---");
+        // console.log(response + " messages were sent successfully");
+      });
     // Replace this with your FCM or custom notification logic
 
     // Respond with success or an error message
     res.json({ message: "Push notification sent successfully" });
   } catch (error) {
-    console.log(error,"error");
+    console.log(error, "error");
     res.status(500).json({ error: "Error sending push notification" });
   }
 });
@@ -50,8 +53,3 @@ app.post("/send-notification", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-
-
-
-
